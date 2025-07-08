24c3991ed12036d650ab81d30d6ee8ef64f40243
Abdullah Masood <mabd051104@gmail.com>
2025-07-09 00:29:46 +0500

    Added quotes.js

---
+++ src/data/quotes.js
@@ -0,0 +1,8 @@
+export const quotes = [
+  { topic: "inspiration", text: "The best way to get started is to quit talking and begin doing." },
+  { topic: "inspiration", text: "Don't let yesterday take up too much of today." },
+  { topic: "inspiration", text: "It's not whether you get knocked down, it's whether you get up." },
+  { topic: "life", text: "Life is what happens when you're busy making other plans." },
+  { topic: "life", text: "Get busy living or get busy dying." },
+  { topic: "life", text: "You have within you right now everything you need to deal with whatever the world can throw at you." },
+];
